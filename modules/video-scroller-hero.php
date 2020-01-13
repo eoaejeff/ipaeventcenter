ab45f819ee1dd57801b3248f91094cf75c0fc537
eoaejeff <jeff@entrolabstudios.com>
2020-01-13 09:22:54 -0500

    Adjust video scroller

--- modules/video-scroller-hero.php
+++ modules/video-scroller-hero.php
@@ -15,7 +15,7 @@ $query = new WP_Query( $args );
 	        var videoId;
 	        videoID = 'qFA2pRbvANU';
 	    </script>
-	 	<div class="video-background">
+	 	<div class="video-background show-for-medium">
 	        <div class="video-foreground" id="video-foreground">
 	            <div id="videoPlayer"></div>
 	        </div>
