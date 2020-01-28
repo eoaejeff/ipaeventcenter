fb562ad1e6d26d4ab500fb0e8005853b720a5e08
eoaejeff <jeff@entrolabstudios.com>
2020-01-28 09:43:31 -0500

    Utility Nav style

--- parts/nav-offcanvas-topbar.php
+++ parts/nav-offcanvas-topbar.php
@@ -7,7 +7,23 @@
 ?>
 
 
-<div class="utility"></div>
+<div class="utility">
+	<div class="grid-x">
+		<div class="cell small-7 phone">
+			<i class="fas fa-phone"></i> CALL <a href="tel:7325778200">732-577-8200 X383</a>
+		</div>
+		<div class="cell small-5 socials text-right">
+			<h4>
+				<a href="https://www.facebook.com/EventsAtiPlayAmerica/?ref=bookmarks" target="_blank"><i style="font-size:24px;" class="fab fa-facebook-square"></i></a>
+				&nbsp;
+				<a href="https://www.linkedin.com/showcase/the-event-center-ipa/" target="_blank"><i style="font-size:24px;" class="fab fa-linkedin-in"></i></a>
+				&nbsp;
+				<a href="https://www.instagram.com/eventcenteratiplayamerica/" target="_blank"><i style="font-size:24px;" class="fab fa-instagram"></i></a>
+				&nbsp;
+			</h4>
+		</div>
+	</div>
+</div>
 
 <div class="top-bar" id="top-bar-menu">
 	
@@ -25,7 +41,7 @@
 	
 	<div class="top-bar-extras show-for-large">
 		
-		<ul class="menu dropdown">
+		<ul class="menu">
 			<li><a href="/about-us/">About Us</a></li>
 			<li><a href="/booking-agents/">Booking Agents</a></li>
 			<li><a href="/testimonials/">Testimonials</a></li> 
