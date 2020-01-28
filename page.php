5184931760a08bd0f30013ac64dc37cae15908c4
eoaejeff <jeff@entrolabstudios.com>
2020-01-28 10:14:58 -0500

    Add gallery option to page

--- page.php
+++ page.php
@@ -33,50 +33,50 @@ get_header(); ?>
 				</main> <!-- end #main -->
 			    
 			</div> <!-- end #inner-content -->
-			
-			<?php $space_gallery_images = get_field( 'event_types_gallery' ); ?>
-<?php if ( $space_gallery_images ) :  ?>
-	<section class="room-gallery">
-		<div class="grid-container">
-			<div class="grid-x grid-margin-x grid-padding-x">
-				<div class="cell">
-					<h2 class="text-center"><?php echo get_field('events_gallery_heading'); ?></h2>
-					<div class="space-gallery">
-						<?php foreach ( $space_gallery_images as $space_gallery_image ): ?>
-							<div class="slide">
-								<img src="<?php echo $space_gallery_image['sizes']['large']; ?>" alt="<?php echo $space_gallery_image['alt']; ?>" />
-							</div>
-						<?php endforeach; ?>
-					</div>
-				</div>
-			</div>
-		</div>
-	</section>
-<?php endif; ?>
-
-<script>
-	jQuery(document).ready(function( $ ) {
-		$('.space-gallery').slick({
-			slidesToShow: 3,		
-			arrows:true,
-			autoplay:true,
-			prevArrow: '<button type="button" class="slick-prev"><i class="fas fa-chevron-circle-left"></i></button>',
-			nextArrow: '<button type="button" class="slick-next"><i class="fas fa-chevron-circle-right"></i></button>',
-			responsive: [
-		    {
-		      breakpoint: 768,
-		      settings: {
-		        slidesToShow: 2
-		      }
-		    }
-		  ]		
-		});
-	});
-</script>
 		
 		</div>
 	
 	</div> <!-- end #content -->
+	
+	<?php $space_gallery_images = get_field( 'event_types_gallery' ); ?>
+		<?php if ( $space_gallery_images ) :  ?>
+			<section class="room-gallery">
+				<div class="grid-container">
+					<div class="grid-x grid-margin-x grid-padding-x">
+						<div class="cell">
+							<h2 class="text-center"><?php echo get_field('events_gallery_heading'); ?></h2>
+							<div class="space-gallery">
+								<?php foreach ( $space_gallery_images as $space_gallery_image ): ?>
+									<div class="slide">
+										<img src="<?php echo $space_gallery_image['sizes']['large']; ?>" alt="<?php echo $space_gallery_image['alt']; ?>" />
+									</div>
+								<?php endforeach; ?>
+							</div>
+						</div>
+					</div>
+				</div>
+			</section>
+		<?php endif; ?>
+		
+		<script>
+			jQuery(document).ready(function( $ ) {
+				$('.space-gallery').slick({
+					slidesToShow: 3,		
+					arrows:true,
+					autoplay:true,
+					prevArrow: '<button type="button" class="slick-prev"><i class="fas fa-chevron-circle-left"></i></button>',
+					nextArrow: '<button type="button" class="slick-next"><i class="fas fa-chevron-circle-right"></i></button>',
+					responsive: [
+				    {
+				      breakpoint: 768,
+				      settings: {
+				        slidesToShow: 2
+				      }
+				    }
+				  ]		
+				});
+			});
+		</script>
 
 <?php get_footer(); ?>
 
