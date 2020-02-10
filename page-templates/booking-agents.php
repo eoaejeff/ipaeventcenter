8b48007d4e66fcc64dc1947dc3e5e4733c563e04
Web Development <webdev@Valerys-Mini.home>
2020-02-10 11:16:14 -0500

    Add booking agents template

--- page-templates/booking-agents.php
+++ page-templates/booking-agents.php
@@ -6,6 +6,8 @@
 	
 ?>
 
+<?php get_template_part('parts/page-header'); ?>
+
 <section class="page-content">
 	<div class="grid-container">
 		<div class="grid-x grid-margin-x grid-padding-x grid-padding-y">
@@ -14,16 +16,8 @@
 			</div>
 			<div class="cell large-3 large-order-1 downloads" >
 				<div class="downloads_inner">
-					<?php if ( $post->ID == 960 ) { ?>
 					<h3>There is always a great event going on at The Event Center @iPA!</h3>
-						<a class="button" style="margin: 25px 0;" href="https://www.iplayamerica.com/concerts-events/" target="_blank" rel="noopener noreferrer">See Full Concert Lineup</a>
-					<?php  } else { ?>
-						<h3>PLAN YOUR EVENT</h3>
-						<p>Our Professional Event Planners will work with you to make the experience of planning your event easy. </p>
-						<p>Helping you to create an <b>unforgettable event</b> is what we do!</p>
-						<p>Call <strong>732-577-8200 EXT.383</strong></p>
-						<a class="button text-center" role="button" href="<?php echo site_url(); ?>/contact-us/" target="_self" rel="noopener noreferrer"> Request Information</a>
-					<?php } ?>		
+					<a class="button" style="margin: 25px 0;" href="https://www.iplayamerica.com/concerts-events/" target="_blank" rel="noopener noreferrer">See Full Concert Lineup</a>	
 				</div>
 			</div>		
 		</div>
